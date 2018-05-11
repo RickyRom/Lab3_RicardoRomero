01eec55f4ad4623174176896ab4bc2cda7c10799
RickyRom <rickyromeros@outlook.com>
2018-05-11 13:34:47 -0600

    creacion del menu

---
+++ lab3_RicardoRomero.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+using std::string;
+
+int main() {
+	int opcion;
+	do{
+		cout<<"Bienvenido al Menu de Opciones"<<endl;
+		cout<<"1.- Ejercicio 1"<<endl;
+		cout<<"2.- Ejercicio 2"<<endl;
+		cout<<"3.- Ejercicio 3"<<endl;
+		cout<<"4.- Salir"<<endl;
+		cin>>opcion;
+		switch(opcion) {
+			
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3: 
+				break;
+			case 4;
+				cout<<"TENGA UN BUEN DIA!"<<endl;
+				break;	
+		}while(opcion != 4);
+
+	}
+
+
+	return 0;
+}
+
+
+
+
